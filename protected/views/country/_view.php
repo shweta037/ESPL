f779c586f6031b1b1043c61d69ef4bf707932b7b
shweta037 <shashi.s@troology.com>
2018-10-03 10:32:45 +0530

    /**************Added Country views********/

---
+++ protected/views/country/_view.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this CountryController */
+/* @var $data Country */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('is_deleted')); ?>:</b>
+	<?php echo CHtml::encode($data->is_deleted); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('created_date')); ?>:</b>
+	<?php echo CHtml::encode($data->created_date); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('modified_date')); ?>:</b>
+	<?php echo CHtml::encode($data->modified_date); ?>
+	<br />
+
+
+</div>
