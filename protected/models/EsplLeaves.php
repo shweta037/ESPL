f37e11a47f1909a420fd3cf9166e5080f6869e39
shweta037 <shashi.s@troology.com>
2018-10-11 11:01:21 +0530

    /***************Added model for espl leaves ************/

---
+++ protected/models/EsplLeaves.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * This is the model class for table "espl_leaves".
+ *
+ * The followings are the available columns in table 'espl_leaves':
+ * @property integer $id
+ * @property string $leave_name
+ * @property string $total_leaves
+ * @property string $created_date
+ * @property string $modified_date
+ */
+class EsplLeaves extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'espl_leaves';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('leave_name, total_leaves, created_date, modified_date', 'required'),
+			array('leave_name, total_leaves', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, leave_name, total_leaves, created_date, modified_date', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'leave_name' => 'Leave Name',
+			'total_leaves' => 'Total Leaves',
+			'created_date' => 'Created Date',
+			'modified_date' => 'Modified Date',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('leave_name',$this->leave_name,true);
+		$criteria->compare('total_leaves',$this->total_leaves,true);
+		$criteria->compare('created_date',$this->created_date,true);
+		$criteria->compare('modified_date',$this->modified_date,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return EsplLeaves the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
