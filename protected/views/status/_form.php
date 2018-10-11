6212c3b53430ddfaf065fdc07cfe1a4d3d26903c
shweta037 <shashi.s@troology.com>
2018-10-11 09:48:20 +0530

    /***************changed the styling************/

--- protected/views/status/_form.php
+++ protected/views/status/_form.php
@@ -3,8 +3,21 @@
 /* @var $model Status */
 /* @var $form CActiveForm */
 ?>
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10">
 
-<div class="form">
+
+                <div class="form">
+                    <div class="card ">
+                        <div class="card-header card-header-rose card-header-icon">
+                            <div class="card-icon">
+                                <i class="material-icons">mail_outline</i>
+                            </div>
+                            <h4 class="card-title">Create Project Region</h4>
+                        </div>
+
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'status-form',
@@ -15,23 +28,28 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+                        <div class="card-body ">
+                            <div class="form-group">
+                                <?php echo $form->errorSummary($model); ?>
 
-	<?php echo $form->errorSummary($model); ?>
+
+
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'status_name'); ?>
 
-        <?php echo $form->textField($model,'status_name',array('size'=>60,'maxlength'=>255)); ?>
+        <?php echo $form->textField($model,'status_name',array('size'=>60,'maxlength'=>255,'class'=>"form-control", 'id'=>"region_name",'required'=>"true")); ?>
 		<?php echo $form->error($model,'status_name'); ?>
 	</div>
 
 
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array('class'=>"btn btn-rose")); ?>
 	</div>
 
-<?php $this->endWidget(); ?>
+<?php $this->endWidget();?>
 
-</div><!-- form -->
+                            </div><!-- form -->
+                        </div>
+                    </div></div></div></div></div></div>
