cb0e9377e8b78ef8cc07fde37e06bfe21431bb50
shweta037 <shashi.s@troology.com>
2018-10-11 11:02:37 +0530

    /***************Added search form for espl leaves ************/

---
+++ protected/views/esplLeaves/_search.php
@@ -0,0 +1,45 @@
+<?php
+/* @var $this EsplLeavesController */
+/* @var $model EsplLeaves */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'leave_name'); ?>
+		<?php echo $form->textField($model,'leave_name',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'total_leaves'); ?>
+		<?php echo $form->textField($model,'total_leaves',array('size'=>60,'maxlength'=>255)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'created_date'); ?>
+		<?php echo $form->textField($model,'created_date'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'modified_date'); ?>
+		<?php echo $form->textField($model,'modified_date'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
