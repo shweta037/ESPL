e4a184a90d11359f9a4095a453a126fcb6b2139b
shweta037 <shashi.s@troology.com>
2018-10-23 13:02:23 +0530

    /***********added the created_by to the database***************/

--- protected/models/Users.php
+++ protected/models/Users.php
@@ -10,12 +10,24 @@
  * @property string $password
  * @property string $created_date
  * @property string $modified_date
+ * @property string $name
+  * @property string $role
+  * @property string $created_by
+ *  @property string $active_status
+
  */
 class Users extends CActiveRecord
 {
 	/**
 	 * @return string the associated database table name
 	 */
+    public $name;
+    public $title;
+    public $mobile_number;
+    public $profile_image;
+    public $active_status;
+    public $role;
+    public $created_by;
 	public function tableName()
 	{
 		return 'users';
@@ -33,7 +45,7 @@ class Users extends CActiveRecord
 			array('username, email, password', 'length', 'max'=>255),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, username, email, password,', 'safe', 'on'=>'search'),
+			array('id, username, email, password,created_date,modified_date,name,role,role_id', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -45,12 +57,15 @@ class Users extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+
 		);
 	}
 
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
+
+
 	public function attributeLabels()
 	{
 		return array(
@@ -58,8 +73,14 @@ class Users extends CActiveRecord
 			'username' => 'Username',
 			'email' => 'Email',
 			'password' => 'Password',
-			//'user_role' => 'User Role',
-//			'modified_date' => 'Modified Date',
+            'name'=>'Name',
+            'title'=>'Title',
+            'role'=>'Role',
+            'mobile_number'=>'Mobile Number',
+            'active_status'=>'Status',
+            'created_by'=>'Created By',
+            'created_date'=>'Created Date',
+			'modified_date' => 'Modified Date',
 		);
 	}
 
@@ -78,13 +99,29 @@ class Users extends CActiveRecord
 	public function search()
 	{
 		// @todo Please modify the following code to remove attributes that should not be searched.
+     $id=   Yii::app()->user->getId();
 
+   //  echo $id;
 		$criteria=new CDbCriteria;
-
+        $criteria->alias = 'u';
+        $criteria->select = 'e.created_by,r.description as role,u.id,u.username,u.email,u.password,e.name,e.mobile_number,e.profile_image,e.active_status,e.title,u.created_date,u.modified_date';
 		$criteria->compare('id',$this->id);
 		$criteria->compare('username',$this->username,true);
 		$criteria->compare('email',$this->email,true);
 		$criteria->compare('password',$this->password,true);
+       // $criteria->compare('created_by',$this->created_by,true);
+        $criteria->compare('created_date',$this->created_date,true);
+        $criteria->compare('modified_date',$this->modified_date,true);
+       // $criteria->compare('name',$this->name,true);
+        $criteria->group = 'u.id';
+
+
+        $criteria->join= 'JOIN espl_employee_details e ON  (e.user_id=u.id) Left JOIN role r ON  (r.id=u.role_id)';
+      if( Yii::app()->user->getState('role') !="Admin"){
+        $criteria->addCondition('u.id = ' . $id);//this is where condition
+    }
+       // $criteria->together = true;
+        $models = Users::model()->find($criteria);
 		//$criteria->compare('user_role',$this->user_role,true);
 		//$criteria->compare('modified_date',$this->modified_date,true);
 
