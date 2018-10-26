58700c6ecbafdaa81b034788f8b6d72c9681b5ab
Rama <rama@vmail.in>
2018-10-26 10:10:03 +0530

    /*************added the created_by fields **************/

--- protected/views/esplProposal/admin.php
+++ protected/views/esplProposal/admin.php
@@ -3,13 +3,13 @@
 /* @var $model EsplProposal */
 
 $this->breadcrumbs=array(
-	'Espl Proposals'=>array('index'),
-	'Manage',
+    'Espl Proposals'=>array('index'),
+    'Manage',
 );
 
 $this->menu=array(
-	array('label'=>'List EsplProposal', 'url'=>array('index')),
-	array('label'=>'Create EsplProposal', 'url'=>array('create')),
+    array('label'=>'List EsplProposal', 'url'=>array('index')),
+    array('label'=>'Create EsplProposal', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -51,48 +51,57 @@ $('.search-form form').submit(function(){
 
 
                         <?php //echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+                        <div class="search-form" style="display:none">
+                            <?php $this->renderPartial('_search',array(
+                                'model'=>$model,
+                            )); ?>
+                        </div><!-- search-form -->
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'espl-proposal-grid',
-    'itemsCssClass'=>'table table-striped table-no-bordered table-hover dataTable dtr-inline',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		//'id',
-		'project_title',
-		'proposa_revision_number',
-		'proposal_issue_date',
-        'attachment_image',
-        'status_name',
-       /* array(
-            'header' => 'Created By',
-            'value' => Yii::app()->user->getState('role'),
-        ),*/
-       'created_by',
-        'created_date',
-        'modified_date',
-
-
-        array(
-            'class'=>'CButtonColumn',
-            'header' => 'Action',
-            'template'=>'{update}',
-            'buttons'=>array(
-
-                'update'=>array(
-                    'label'=>'<i class="material-icons"></i>',
-                    'htmlOptions'=>array('style'=>'width: 50px; text-align: center;', 'class'=>'zzz'),
-                    'option'=>array('class'=>'btn btn-link btn-info btn-just-icon like')
-                ),
-
-            ),
+                        <?php $this->widget('zii.widgets.grid.CGridView', array(
+                            'id'=>'espl-proposal-grid',
+                            'itemsCssClass'=>'table table-striped table-no-bordered table-hover dataTable dtr-inline',
+                            'dataProvider'=>$model->search(),
+                            'filter'=>$model,
+                            'columns'=>array(
+                                //'id',
+                                'project_title',
+                                'proposa_revision_number',
+                                'proposal_issue_date',
+                                //'attachment_image',
+                                'status_name',
+                                'contract_value',
+                                /* array(
+                                     'header' => 'Created By',
+                                     'value' => Yii::app()->user->getState('role'),
+                                 ),*/
+                                'created_by',
+                                'created_date',
+                                //'modified_date',
 
 
-        ),
-	),
-)); ?>
+                                array(
+                                    'class'=>'CButtonColumn',
+                                    'header' => 'Action',
+                                    'template'=>'{update1}',
+                                    'buttons'=>array(
+                                        'update1'=>array(
+                                            'label'=>'<input class="btn btn-primary" type="button" name="yt0" value="Edit">',
+                                            'htmlOptions'=>array('style'=>'width: 50px; text-align: center;', 'class'=>'zzz'),
+                                            'visible'=>'$data->proposal_status==3?false:true',
+                                            'option'=>array('class'=>'btn btn-link btn-info btn-just-icon like'),
+                                            'url'   => 'Yii::app()->createUrl("EsplProposal/update", array("id" => $data->id))',
+
+                                        ),
+
+                                    ),
+
+
+                                ),
+                            ),
+                        )); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
